f1a047865df01fc1fbde70d8517571d46bb28d31
Eric Ngako <ericnk1@live.com>
2021-03-17 21:38:39 +0200

    cors issue with heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
   development: true,
   environmentName: 'DEV',
-  baseURL: 'https://petclinic-spring-boot.herokuapp.com/' // 'http://localhost:8080/'
+  baseURL: 'https://employee-management-app1.herokuapp.com/' // 'http://localhost:8080/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   development: true,
   environmentName: 'DEV',
-  baseURL: 'https://petclinic-spring-boot.herokuapp.com/' // 'http://localhost:8080/'
+  baseURL: 'https://employee-management-app1.herokuapp.com/' // 'http://localhost:8080/'
 };
 
 /*
